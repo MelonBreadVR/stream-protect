1e192cac0614b166d2e64483eedc3401335c06d1
Adrian Chen <adrian.evangelion@gmail.com>
2018-04-29 20:48:07 +0100

    ADD - init atom package

---
+++ spec/stream-protect-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import StreamProtectView from '../lib/stream-protect-view';
+
+describe('StreamProtectView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
